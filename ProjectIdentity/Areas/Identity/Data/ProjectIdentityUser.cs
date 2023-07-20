9f4b601de220177c65c1150012eb3869c216e070
Dishhome <Dishhome@DESKTOP-LLD7KLT>
2023-07-20 03:06:02 -0700

    Add project files.

---
+++ ProjectIdentity/Areas/Identity/Data/ProjectIdentityUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProjectIdentity.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ProjectIdentityUser class
+public class ProjectIdentityUser : IdentityUser
+{
+}
+
